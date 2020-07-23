02da5aa4cf886e4b4b886949308f54101801d63b
deepakduseja <deepak.duseja73@gmail.com>
2020-07-23 21:04:54 +0530

    Add files via upload

---
+++ ChangePassword.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+namespace InventoryERPApplication
+{
+    public partial class ChangePassword : DevExpress.XtraEditors.XtraForm
+    {
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        SqlConnection connstring;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+        //   DataTable dt = new DataTable();
+        int i, no = 0;
+        string query;
+        SqlConnection con;
+        SqlCommand cmd;
+        Connection c = new Connection();
+        public ChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (USERNAME.Text == "")
+            {
+                DevExpress.XtraEditors.XtraMessageBox.Show(this, "Enter the User Name !!!", ProductName, MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                USERNAME.Focus();
+                return;
+            }
+            if (NEW.Text == "")
+            {
+                DevExpress.XtraEditors.XtraMessageBox.Show(this, "Enter the New Password !!!", ProductName, MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                NEW.Focus();
+                return;
+            }
+            if (OLD.Text == "")
+            {
+                DevExpress.XtraEditors.XtraMessageBox.Show(this, "Enter the Confirm Password !!!", ProductName, MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                OLD.Focus();
+                return;
+            }
+            try
+            {
+                //c.connopen();
+                //query = "select * from UserLog where UserName='" + USERNAME.Text + "' and Password='" + OLD.Text + "'";
+                //cmd = new SqlCommand(query, c.conn);
+                //dr = cmd.ExecuteReader();
+                if (NEW.Text == OLD.Text)
+                {
+                    c.connopen();
+                    query = "Update UserLog set Password=@Password where UserName=@UserName";
+                    cmd = new SqlCommand(query, c.conn);
+                    cmd.Parameters.Add("@Password", SqlDbType.NVarChar).Value = NEW.Text;
+                    cmd.Parameters.Add("@UserName", SqlDbType.NVarChar).Value = USERNAME.Text;
+                    cmd.ExecuteNonQuery();
+                    DevExpress.XtraEditors.XtraMessageBox.Show(this, "Passoword Reset Successfully.", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    NEW.Text = "";
+                    OLD.Text = "";
+                    USERNAME.Text = "";
+                }
+                else
+                {
+                    DevExpress.XtraEditors.XtraMessageBox.Show(this, "Password Mismastch !!!", ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                DevExpress.XtraEditors.XtraMessageBox.Show(this, "Error:" + ex.ToString(), ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                c.coonclose();
+            }
+        }
+
+        private void simpleButton4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void USERNAME_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           // e.KeyChar = (e.KeyChar.ToString()).ToUpper().ToCharArray()[0];
+        }
+        private void NEW_Validating(object sender, CancelEventArgs e)
+        {
+            //Regex regex = new Regex("^[A-Z][0-9]*$");
+            //if (regex.IsMatch(NEW.Text))
+            //{
+            //    errorProvider1.SetError(NEW, String.Empty);
+            //}
+            //else
+            //{
+            //    errorProvider1.SetError(NEW,
+            //        "Both Alphabet and numbers may be entered here");
+            //}
+        }
+
+        private void CHANGEPASSWORD_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            NEW.Text = "";
+            OLD.Text = "";
+            USERNAME.Text = "";
+
+        }
+    }
+}
---
+++ ChangePassword.designer.cs
@@ -0,0 +1,190 @@
+﻿namespace InventoryERPApplication
+{
+    partial class ChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.USERNAME = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.OLD = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.NEW = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.USERNAME.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OLD.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NEW.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // USERNAME
+            // 
+            this.USERNAME.Location = new System.Drawing.Point(554, 12);
+            this.USERNAME.Name = "USERNAME";
+            this.USERNAME.Properties.Appearance.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.USERNAME.Properties.Appearance.Options.UseFont = true;
+            this.USERNAME.Size = new System.Drawing.Size(248, 26);
+            this.USERNAME.TabIndex = 0;
+            this.USERNAME.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.USERNAME_KeyPress);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Nyala", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(431, 17);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(71, 17);
+            this.labelControl1.TabIndex = 3;
+            this.labelControl1.Text = "User Name";
+            // 
+            // OLD
+            // 
+            this.OLD.Location = new System.Drawing.Point(554, 44);
+            this.OLD.Name = "OLD";
+            this.OLD.Properties.Appearance.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OLD.Properties.Appearance.Options.UseFont = true;
+            this.OLD.Properties.PasswordChar = '*';
+            this.OLD.Properties.UseSystemPasswordChar = true;
+            this.OLD.Size = new System.Drawing.Size(248, 26);
+            this.OLD.TabIndex = 1;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Nyala", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(431, 49);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(59, 17);
+            this.labelControl2.TabIndex = 5;
+            this.labelControl2.Text = "Password";
+            // 
+            // NEW
+            // 
+            this.NEW.Location = new System.Drawing.Point(554, 76);
+            this.NEW.Name = "NEW";
+            this.NEW.Properties.Appearance.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NEW.Properties.Appearance.Options.UseFont = true;
+            this.NEW.Properties.PasswordChar = '*';
+            this.NEW.Properties.UseSystemPasswordChar = true;
+            this.NEW.Size = new System.Drawing.Size(248, 26);
+            this.NEW.TabIndex = 2;
+            this.NEW.Validating += new System.ComponentModel.CancelEventHandler(this.NEW_Validating);
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Nyala", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Location = new System.Drawing.Point(431, 81);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(117, 17);
+            this.labelControl3.TabIndex = 7;
+            this.labelControl3.Text = "Confirm Password";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.Appearance.Font = new System.Drawing.Font("Nyala", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton4.Appearance.Options.UseFont = true;
+            this.simpleButton4.Location = new System.Drawing.Point(726, 108);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(76, 27);
+            this.simpleButton4.TabIndex = 23;
+            this.simpleButton4.Text = "EXIT";
+            this.simpleButton4.Click += new System.EventHandler(this.simpleButton4_Click);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Appearance.Font = new System.Drawing.Font("Nyala", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton3.Appearance.Options.UseFont = true;
+            this.simpleButton3.Location = new System.Drawing.Point(641, 108);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(79, 27);
+            this.simpleButton3.TabIndex = 22;
+            this.simpleButton3.Text = "RESET";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Nyala", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.Location = new System.Drawing.Point(554, 108);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(81, 27);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "SAVE";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // ChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1272, 500);
+            this.Controls.Add(this.simpleButton4);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.NEW);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.OLD);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.USERNAME);
+            this.Controls.Add(this.labelControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ChangePassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CHANGEPASSWORDcs";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.CHANGEPASSWORD_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.USERNAME.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OLD.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NEW.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.TextEdit USERNAME;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.TextEdit OLD;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit NEW;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
